c76aac4fac610d31d0d23688ae13a50a40e390b1
Dmitry913HUB <gmpochtcript@mail.ru>
2023-02-19 00:20:46 +0300

    formSubmit fix fetch

--- ZadTen/submitForm/submitForm.js
+++ ZadTen/submitForm/submitForm.js
@@ -1,11 +1,5 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-    window.iframe.onload = function(){
-        pushFromServer();
-      }
-  });
-
 let url = 'http://178.216.77.25/fileToServer';
 
 function pushToServer(){
@@ -16,6 +10,8 @@ function pushToServer(){
     // form.enctype = 'multipart/form-data';
 
     // form.submit();
+    let formData = document.getElementById('formSubmit');
+    //alert(formData.toString())
     postData(url, pushFromServer)
 }
 
@@ -25,14 +21,19 @@ function pushFromServer(formParam){
 
 async function postData( url = '', onOk){
 
+    let form = new FormData(document.getElementById('formSubmit'));
+    //form.append('uname', 'Nikita');
+    //form.append('surname', 'Ten');
+    
     let response = await fetch(url, {
         method: 'POST',
+        mode: 'no-cors',
         // headers: {
         //     'Content-Type': 'multipart/form-data'
         //     // 'Content-Type': 'application/json'
         //     // 'Content-Type': 'application/x-www-form-urlencoded',
         // },
-        body: new FormData() // body data type must match "Content-Type" header
+        body: form // body data type must match "Content-Type" header
     });
 
     if (response.ok) { // если HTTP-статус в диапазоне 200-299
