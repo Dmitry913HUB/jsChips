d2fa578a84609476ffe8e282929d94d9f34247f6
Dmitry913HUB <58403614+Dmitry913HUB@users.noreply.github.com>
2023-03-02 00:12:18 +0300

    zadten -> submitform -> attempts to fix

--- ZadTen/submitForm/submitForm.js
+++ ZadTen/submitForm/submitForm.js
@@ -2,6 +2,62 @@
 
 let url = 'http://178.216.77.25/fileToServer';
 
+// function pushFromServer(formParam){
+//     console.log(formParam);
+// }
+
+// async function postData( url = ''){
+
+//     let form = new FormData(document.getElementById('formSubmit'));
+//     //form.append('uname', 'Nikita');
+//     //form.append('surname', 'Ten');
+    
+//     let response = await fetch(url, {
+//         method: 'POST',
+//         mode: 'no-cors',
+//         // headers: {
+//         //     'Content-Type': 'multipart/form-data'
+//         //     // 'Content-Type': 'application/json'
+//         //     // 'Content-Type': 'application/x-www-form-urlencoded',
+//         // },
+//         body: form // body data type must match "Content-Type" header
+//     });
+
+//     let awaitResponse = await response.json();
+
+//     if (response.ok) { // если HTTP-статус в диапазоне 200-299
+//         // получаем тело ответа (см. про этот метод ниже)
+//         //onOk(response.json()); // parses JSON response into native JavaScript objects
+//         return awaitResponse;
+//       }
+
+//     return JSON.stringify("Ошибка HTTP: " + response.status);     
+// }
+
+async function postData( url = ''){
+
+    let form = new FormData(document.getElementById('formSubmit'));
+    //form.append('uname', 'Nikita');
+    //form.append('surname', 'Ten');
+    
+    // fetch(url, {
+    //     method: 'POST',
+    //     mode: 'no-cors',
+    //     // headers: {
+    //     //     'Content-Type': 'multipart/form-data'
+    //     //     // 'Content-Type': 'application/json'
+    //     //     // 'Content-Type': 'application/x-www-form-urlencoded',
+    //     // },
+    //     body: form // body data type must match "Content-Type" header
+    // }).then(response => response.json()).then(res => console.log(res));
+    // //}).then(response => console.log(response.headers));
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(res => console.log(res.res));
+   
+}
+
 function pushToServer(){
     // let form = document.getElementById('formSubmit');
 
@@ -12,33 +68,8 @@ function pushToServer(){
     // form.submit();
     let formData = document.getElementById('formSubmit');
     //alert(formData.toString())
-    postData(url, pushFromServer)
-}
-
-function pushFromServer(formParam){
-    console.log(formParam);
-}
-
-async function postData( url = '', onOk){
-
-    let form = new FormData(document.getElementById('formSubmit'));
-    //form.append('uname', 'Nikita');
-    //form.append('surname', 'Ten');
-    
-    let response = await fetch(url, {
-        method: 'POST',
-        mode: 'no-cors',
-        // headers: {
-        //     'Content-Type': 'multipart/form-data'
-        //     // 'Content-Type': 'application/json'
-        //     // 'Content-Type': 'application/x-www-form-urlencoded',
-        // },
-        body: form // body data type must match "Content-Type" header
-    });
-
-    if (response.ok) { // если HTTP-статус в диапазоне 200-299
-        // получаем тело ответа (см. про этот метод ниже)
-        onOk(response.json()); // parses JSON response into native JavaScript objects
-      }
-    //return JSON.stringify("Ошибка HTTP: " + response.status);     
+    // postData(url).then((data) => {
+    //     console.log(data);
+    // });
+    postData(url);
 }
