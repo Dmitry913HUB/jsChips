38492da64319971e3ab9b566a58276656b9c60d1
Dmitry913HUB <gmpochtcript@mail.ru>
2023-04-07 13:34:48 +0300

    DataTypes -> array -> arrayMethods zadacha 13

--- DataTypes/array/arrayMethods.js
+++ DataTypes/array/arrayMethods.js
@@ -651,9 +651,192 @@
 
     // zadacha 6
 
-    
+    function Calculator(){
+        this.methods = {
+            '-': (a, b) => a - b,
+            '+': (a, b) => a + b,
+        };
 
-    
+        this.calculate = (str) => {
+            let split = str.split(' ');
+            a = +split[0];
+            b = +split[2];
+            op = split[1];
+
+            if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+                return NaN;
+            }
+
+            return this.methods[op](a, b);
+        };
+
+        this.addMethod = (name, func) => {
+            this.methods[name] = func;
+        }
+    }
+
+    let powerCalc = new Calculator;
+    powerCalc.addMethod("*", (a, b) => a * b);
+    powerCalc.addMethod("/", (a, b) => a / b);
+    powerCalc.addMethod("**", (a, b) => a ** b);
+
+    let result = powerCalc.calculate("2 ** 3");
+    alert( result ); // 8
+
+    // zadacha 7
+
+    let vasya = { name: "Вася", age: 25 };
+    let petya = { name: "Петя", age: 30 };
+    let masha = { name: "Маша", age: 28 };
+
+    let users = [ vasya, petya, masha ];
+
+    let names = users.map(item => item.name);
+
+    alert( names ); // Вася, Петя, Маша
+
+    // zadacha 8
+    {
+
+        let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+        let petya = { name: "Петя", surname: "Иванов", id: 2 };
+        let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+        let users = [ vasya, petya, masha ];
+
+        let usersMapped = users.map((item) => ({
+            fullName: `${item.name} ${item.surname}`, 
+            id: item.id,
+        }));
+
+        /*
+        usersMapped = [
+        { fullName: "Вася Пупкин", id: 1 },
+        { fullName: "Петя Иванов", id: 2 },
+        { fullName: "Маша Петрова", id: 3 }
+        ]
+        */
+
+        alert( usersMapped[0].id ) // 1
+        alert( usersMapped[0].fullName ) // Вася Пупкин        
+    }
+
+    // zadacha 9
+
+    {
+        function sortByAge(arr){
+            arr.sort((a, b) => a.age - b.age)
+        }
+        
+        let vasya = { name: "Вася", age: 25 };
+        let petya = { name: "Петя", age: 30 };
+        let masha = { name: "Маша", age: 28 };
+
+        let arr = [ vasya, petya, masha ];
+
+        sortByAge(arr);
+
+        // теперь: [vasya, masha, petya]
+        alert(arr[0].name); // Вася
+        alert(arr[1].name); // Маша
+        alert(arr[2].name); // Петя
+    }
+
+    // zadacha 10
+
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+          let j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+      }
+      
+      // подсчёт вероятности для всех возможных вариантов
+      let count = {
+        '123': 0,
+        '132': 0,
+        '213': 0,
+        '231': 0,
+        '321': 0,
+        '312': 0
+      };
+      
+      for (let i = 0; i < 1000000; i++) {
+        let array = [1, 2, 3];
+        shuffle(array);
+        count[array.join('')]++;
+      }
+      
+      // показать количество всех возможных вариантов
+      for (let key in count) {
+        alert(`${key}: ${count[key]}`);
+      }
+
+    // zadacha 11
+
+    {
+        function getAverageAge(arr){
+            return arr.reduce((sum, current) => sum + current.age, 0) / users.length;
+        }
+
+        let vasya = { name: "Вася", age: 25 };
+        let petya = { name: "Петя", age: 30 };
+        let masha = { name: "Маша", age: 29 };
+
+        let arr = [ vasya, petya, masha ];
+
+        alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+    }
+
+    // zadacha 12
+
+    {
+        function unique(arr) {
+            let newArr = [];            
+            for(let str of arr){
+                if(!newArr.includes(str)){
+                    newArr.push(str);
+                }
+            }
+            return newArr;
+        }
+          
+          let strings = ["кришна", "кришна", "харе", "харе",
+            "харе", "харе", "кришна", "кришна", ":-O"
+          ];
+          
+          alert( unique(strings) ); // кришна, харе, :-O
+    }
+
+    // zadacha 13
+
+    {
+        function groupById(users){
+            return users.reduce((obj, value) => {
+                obj[value.id] = value;
+                return obj;
+            }, {});
+        }
+
+        let users = [
+            {id: 'john', name: "John Smith", age: 20},
+            {id: 'ann', name: "Ann Smith", age: 24},
+            {id: 'pete', name: "Pete Peterson", age: 31},
+          ];
+          
+          let usersById = groupById(users);
+          
+          /*
+          // после вызова у нас должно получиться:
+          
+          usersById = {
+            john: {id: 'john', name: "John Smith", age: 20},
+            ann: {id: 'ann', name: "Ann Smith", age: 24},
+            pete: {id: 'pete', name: "Pete Peterson", age: 31},
+          }
+          */
+    }
+        
 }
 /*---------------------------------------*/
 
