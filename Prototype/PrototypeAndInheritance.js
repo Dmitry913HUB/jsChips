2f80b233a366549b33c3533f78475a40504e0e9c
Dmitry913HUB <58403614+Dmitry913HUB@users.noreply.github.com>
2023-06-10 15:26:43 +0300

    Prototype -> PrototypeAndInheritance

--- Prototype/PrototypeAndInheritance.js
+++ Prototype/PrototypeAndInheritance.js
@@ -265,8 +265,143 @@
 
 }
 /*---------------------------------------*/
-/*          итого          */
+/*          Задачи          */
 {
+
+    // Работа с прототипами
+
+    let animal = {
+        jumps: null
+      };
+      let rabbit = {
+        __proto__: animal,
+        jumps: true
+      };
+      
+      alert( rabbit.jumps ); // true (1)
+      
+      delete rabbit.jumps;
+      
+      alert( rabbit.jumps ); // null (2)
+      
+      delete animal.jumps;
+      
+      alert( rabbit.jumps ); // undefined (3)
+
+    // Алгоритм поиска
+
+    let head = {
+        glasses: 1
+      };
+      
+      let table = {
+        pen: 3,
+        __proto__: head,
+      };
+      
+      let bed = {
+        sheet: 1,
+        pillow: 2,
+        __proto__: table,
+      };
+      
+      let pockets = {
+        money: 2000,
+        __proto__: bed,
+      };
+
+    // Куда будет произведена запись?
+
+    animal = {
+        eat() {
+          this.full = true;
+        }
+      };
+      
+    rabbit = {
+        __proto__: animal
+      };
+      
+    rabbit.eat(); // rabbit full
+
+    // Почему наедаются оба хомяка?
+
+    // Давайте внимательно посмотрим, что происходит при вызове speedy.eat("apple").
+
+    // Сначала в прототипе (=hamster) находится метод speedy.eat, 
+    // а затем он выполняется с this=speedy (объект перед точкой).
+
+    // Затем в this.stomach.push() нужно найти свойство stomach и вызвать для него push. 
+    // Движок ищет stomach в this (=speedy), но ничего не находит.
+
+    // Он идёт по цепочке прототипов и находит stomach в hamster.
+
+    // И вызывает для него push, добавляя еду в живот прототипа.
+
+    // Получается, что у хомяков один живот на двоих!
+
+    // И при lazy.stomach.push(...) и при speedy.stomach.push(), 
+    // свойство stomach берётся из прототипа (так как его нет в самом объекте), 
+    // затем в него добавляются данные.
+
+    // Обратите внимание, что этого не происходит при простом присваивании this.stomach=:
+
+    let hamster = {
+    stomach: [],
+
+    eat(food) {
+        // присвоение значения this.stomach вместо вызова this.stomach.push
+        this.stomach = [food];
+    }
+    };
+
+    let speedy = {
+    __proto__: hamster
+    };
+
+    let lazy = {
+    __proto__: hamster
+    };
+
+    // Шустрый хомяк нашёл еду
+    speedy.eat("apple");
+    alert( speedy.stomach ); // apple
+
+    // Живот ленивого хомяка пуст
+    alert( lazy.stomach ); // <ничего>
+
+    // Теперь всё работает правильно, потому что this.stomach= не ищет свойство stomach. 
+    // Значение записывается непосредственно в объект this.
+
+    // Также мы можем полностью избежать проблемы, если у каждого хомяка будет собственный живот:
+
+    hamster = {
+    stomach: [],
+
+    eat(food) {
+        this.stomach.push(food);
+    }
+    };
+
+    speedy = {
+    __proto__: hamster,
+    stomach: []
+    };
+
+    lazy = {
+    __proto__: hamster,
+    stomach: []
+    };
+
+    // Шустрый хомяк нашёл еду
+    speedy.eat("apple");
+    alert( speedy.stomach ); // apple
+
+    // Живот ленивого хомяка пуст
+    alert( lazy.stomach ); // <ничего>
+
+    // Все свойства, описывающие состояние объекта (как свойство stomach в примере выше), 
+    // рекомендуется записывать в сам этот объект. Это позволяет избежать подобных проблем.
 
 }
 /*---------------------------------------*/
